1d2d9c195c4fdee71c708a49b5a47d45d7db8cf5
Germano Ribeiro <germanno.verde@gmail.com>
2024-01-01 19:16:51 -0300

    projecto movies lib

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import { Outlet }  from 'react-router-dom';
+import Navbar from './components/Navbar';
+import './App.css';
+
+function App() {
+
+  return (
+
+     <div className='app'>
+   <Navbar/>
+        <h2>Movies Lib</h2>
+        <Outlet/>
+</div>
+   
+  );
+}
+
+export default App;
